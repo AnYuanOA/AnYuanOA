b9d118acb784d128e039a62fb06e70801e87dd24
彭侃 <wh_pengkan@163.com>
2017-12-26 08:11:39 +0800

    待办待阅列表动态化

--- pages/todo/todo.js
+++ pages/todo/todo.js
@@ -11,7 +11,155 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showType: TodoType.Todo
+    showType: TodoType.Todo,
+    todoList: [
+      {
+        typeTitle:'行政用章申请',
+        title:'测试。',
+        publish_name:'测试1',
+        publish_time:'2017-11-29 15:53:24',
+        level:'紧急'
+      },
+      {
+        typeTitle: '项目开工通知单',
+        title: '测试开工。',
+        publish_name: 'jinher',
+        publish_time: '2017-11-29 15:53:24',
+        level: '一般'
+      },
+      {
+        typeTitle: '合同会签申请',
+        title: '测试。',
+        publish_name: '测试1',
+        publish_time: '2017-11-29 15:53:24',
+        level: '紧急'
+      },
+      {
+        typeTitle: '项目风险分析(产品要求评审)记录单',
+        title: '测试流程。',
+        publish_name: '测试1',
+        publish_time: '2017-11-29 15:53:24',
+        level: '一般'
+      },
+      {
+        typeTitle: '行政用章申请',
+        title: '测试。',
+        publish_name: '测试1',
+        publish_time: '2017-11-29 15:53:24',
+        level: '紧急'
+      },
+      {
+        typeTitle: '项目开工通知单',
+        title: '测试开工。',
+        publish_name: 'jinher',
+        publish_time: '2017-11-29 15:53:24',
+        level: '一般'
+      },
+      {
+        typeTitle: '合同会签申请',
+        title: '测试。',
+        publish_name: '测试1',
+        publish_time: '2017-11-29 15:53:24',
+        level: '紧急'
+      },
+      {
+        typeTitle: '项目风险分析(产品要求评审)记录单',
+        title: '测试流程。',
+        publish_name: '测试1',
+        publish_time: '2017-11-29 15:53:24',
+        level: '一般'
+      },
+      {
+        typeTitle: '行政用章申请',
+        title: '测试。',
+        publish_name: '测试1',
+        publish_time: '2017-11-29 15:53:24',
+        level: '紧急'
+      },
+      {
+        typeTitle: '项目开工通知单',
+        title: '测试开工。',
+        publish_name: 'jinher',
+        publish_time: '2017-11-29 15:53:24',
+        level: '一般'
+      },
+      {
+        typeTitle: '合同会签申请',
+        title: '测试。',
+        publish_name: '测试1',
+        publish_time: '2017-11-29 15:53:24',
+        level: '紧急'
+      },
+      {
+        typeTitle: '项目风险分析(产品要求评审)记录单',
+        title: '测试流程。',
+        publish_name: '测试1',
+        publish_time: '2017-11-29 15:53:24',
+        level: '一般'
+      }
+    ],
+    toReadList: [
+      {
+        typeTitle: '发文办理流程',
+        title: '关于开展员工职级调整工作的通知',
+        publish_name: '曾闪'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于发布《2017年管理评审报告》的通知',
+        publish_name: 'jinher'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于发布2017年11月质量月报的通知',
+        publish_name: '测试1'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于公布2017年度“优秀咨询成果奖”评选结果的通知',
+        publish_name: '测试1'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于开展员工职级调整工作的通知',
+        publish_name: '曾闪'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于发布《2017年管理评审报告》的通知',
+        publish_name: 'jinher'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于发布2017年11月质量月报的通知',
+        publish_name: '测试1'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于公布2017年度“优秀咨询成果奖”评选结果的通知',
+        publish_name: '测试1'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于开展员工职级调整工作的通知',
+        publish_name: '曾闪'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于发布《2017年管理评审报告》的通知',
+        publish_name: 'jinher'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于发布2017年11月质量月报的通知',
+        publish_name: '测试1'
+      },
+      {
+        typeTitle: '发文办理流程',
+        title: '关于公布2017年度“优秀咨询成果奖”评选结果的通知',
+        publish_name: '测试1'
+      }
+    ]
   },
 
   /**
@@ -19,55 +167,6 @@ Page({
    */
   onLoad: function (options) {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   },
 
   /*
