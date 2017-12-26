1a4b03eee35008e288fab9f0ba4d787d0c498754
wanbx <287186419@qq.com>
2017-12-26 14:12:50 +0800

    寻呼
    寻呼

--- pages/call/call.js
+++ pages/call/call.js
@@ -1,66 +1,123 @@
-// pages/call/call.js
-Page({
 
+
+// 获取全局应用程序实例对象
+const app = getApp();
+
+if (typeof seekCall == "undefined") {
+  var seekCall = {}
+  seekCall.notRead = 1//未阅
+  seekCall.collect = 2//收藏
+  seekCall.sent = 3//已发
+  seekCall.received = 4//已收
+}
+
+// 创建页面实例对象
+Page({
+  /**
+   * 页面名称
+   */
+  name: "message",
   /**
    * 页面的初始数据
    */
+
   data: {
-  
+    showType: seekCall.notRead
+
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad() {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow() {
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh() {
+
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+
+  //以下为自定义点击事件
+
+  tap_notRead: function (e) {
+
+    if (this.data.seekCall != seekCall.notRead) {
+      this.setData({
+        showType: seekCall.notRead
+      });
+    }
+
+
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+  tap_collect: function (e) {
+    if (this.data.seekCall != seekCall.collect) {
+      this.setData({
+        showType: seekCall.collect
+
+      });
+    }
+
+  },
+
+  tap_sent: function (e) {
+    if (this.data.seekCall != seekCall.sent) {
+      this.setData({
+        showType: seekCall.sent
+      });
+    }
+
+  },
+
+  tap_received: function (e) {
+    if (this.data.seekCall != seekCall.received) {
+      this.setData({
+        showType: seekCall.received
+      });
+    }
+  },
+
+
+  tap_forward: function (e) {
+
+    wx.navigateTo({
+      url: '../callEdit/callEdit'
+    })
+
+
+  },
+
 })
+
