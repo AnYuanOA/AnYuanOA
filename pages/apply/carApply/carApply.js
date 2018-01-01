1e12d2c78899fac0fbcc75a3c8389e9466e72b92
huyang8 <huyang8@jd.com>
2018-01-01 16:19:46 +0800

    安源OA小程序界面设计提交

--- pages/apply/carApply/carApply.js
+++ pages/apply/carApply/carApply.js
@@ -1,11 +1,39 @@
 // pages/apply/carApply/carApply.js
+var date = new Date();
+//年  
+var Y = date.getFullYear();
+//月  
+var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+//日  
+var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+var now = Y + '-' + M + '-' + D;
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-  
+    array: ['用车申请', '休假申请'],
+    index: 0,
+    date: now
+  },
+
+  /**
+   * 监听普通picker选择器
+   */
+  listenerPickerSelected: function (e) {
+    //改变index值，通过setData()方法重绘界面
+    this.setData({
+      index: e.detail.value
+    });
+  }, 
+
+  /**
+   * 监听日期picker选择器
+   */
+  listenerDatePickerSelected: function (e) {
+    this.setData({
+      date: e.detail.value
+    })
   },
 
   /**
