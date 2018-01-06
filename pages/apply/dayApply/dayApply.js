3c36bbefb14b6c220466576c883f629f1f91969a
huyang8 <huyang8@jd.com>
2018-01-06 16:40:26 +0800

    安源OA小程序界面设计提交

--- pages/apply/dayApply/dayApply.js
+++ pages/apply/dayApply/dayApply.js
@@ -12,9 +12,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    array: ['事假', '病假','年假','婚假'],
+    array: ['事假', '病假', '年假', '婚假'],
     index: 0,
-    date: now
+    dateStart: now,
+    dateEnd: now,
   },
 
   /**
@@ -30,9 +31,18 @@ Page({
   /**
    * 监听日期picker选择器
    */
-  listenerDatePickerSelected: function (e) {
+  listenerDateStartPickerSelected: function (e) {
     this.setData({
-      date: e.detail.value
+      dateStart: e.detail.value
+    })
+  },
+
+  /**
+   * 监听日期picker选择器
+   */
+  listenerDateEndPickerSelected: function (e) {
+    this.setData({
+      dateEnd: e.detail.value
     })
   },
 
@@ -90,5 +100,42 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 上传
+   */
+  uploadImg: function (e) {
+    var _this = this;
+    wx.chooseImage({
+      count: 9, // 默认9  
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有  
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
+        _this.setData({
+          tempFilePaths: res.tempFilePaths
+        })
+        var tempFilePath = res.tempFilePaths[0];
+
+      }
+    })
+  },
+
+  /**
+   * 预览
+   */
+  previewImage: function (e) {
+    var that = this,
+      //获取当前图片的下表
+      index = e.currentTarget.dataset.index,
+      //数据源
+      pictures = this.data.tempFilePaths;
+    wx.previewImage({
+      //当前显示下表
+      current: pictures[index],
+      //数据源
+      urls: pictures
+    })
   }
 })
