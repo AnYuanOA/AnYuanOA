b0dc312f4ac705b8f814e07966c2f91cff0b446e
huyang8 <huyang8@jd.com>
2018-01-18 10:41:54 +0800

    安源OA小程序v2版我的页面UI设计提交

--- pages/organize/organize.js
+++ pages/organize/organize.js
@@ -1,49 +1,142 @@
 // pages/organize/organize.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    inputShowed: false,
-    inputVal: "",
-    list: [
+    orgData: [
       {
-        id: '1',
+        code:'001',
+        open: false,
         name: '领导层',
+        empData: [
+          {
+            name: '张三',
+            phone:'19012733888',
+            position: '总经理'
+          }, {
+            name: '李四',
+            phone: '19012733888',
+            position: '副总'
+          }, {
+            name: '王五',
+            phone: '19012733888',
+            position: '副总'
+          }
+        ]
       }, {
-        id: '2',
+        code: '002',
+        open: false,
         name: '安全事业部',
+        empData: [
+          {
+            name: '骞天',
+            phone: '19012733888',
+            position: '主任'
+          }, {
+            name: '烁骞',
+            phone: '19012733888',
+            position: '部长'
+          }, {
+            name: '博俊',
+            phone: '19012733888',
+            position: '专员'
+          }, {
+            name: '芃震',
+            phone: '19012733888',
+            position: '专员'
+          }, {
+            name: '博星',
+            phone: '19012733888',
+            position: '专员'
+          }
+        ]
       }, {
-        id: '3',
+        code: '003',
+        open: false,
         name: '环境事业部',
+        empData: [
+          {
+            name: '腾辰',
+            phone: '19012733888',
+            position: '科长'
+          }, {
+            name: '升驰',
+            phone: '19012733888',
+            position: '专员'
+          }, {
+            name: '梓濡',
+            phone: '19012733888',
+            position: '专员'
+          }
+        ]
       }, {
-        id: '4',
+        code: '004',
+        open: false,
         name: '综合管理部',
+        empData: [
+          {
+            name: '蔚丽',
+            phone: '19012733888',
+            position: '人事经理'
+          }, {
+            name: '薇薇',
+            phone: '19012733888',
+            position: '人事专员'
+          }, {
+            name: '妍鑫',
+            phone: '19012733888',
+            position: 'HRBP'
+          }
+        ]
       }, {
-        id: '5',
+        code: '005',
+        open: false,
         name: '市场部',
+        empData: [
+          {
+            name: '美惠',
+            phone: '19012733888',
+            position: '经理'
+          }, {
+            name: '茜克',
+            phone: '19012733888',
+            position: '部长'
+          }, {
+            name: '柔雪',
+            phone: '19012733888',
+            position: '主任'
+          }, {
+            name: '璐彩',
+            phone: '19012733888',
+            position: '副主任'
+          }, {
+            name: '珊梦',
+            phone: '19012733888',
+            position: '副主任'
+          }, {
+            name: '珠涵',
+            phone: '19012733888',
+            position: '专员'
+          }, {
+            name: '格馨',
+            phone: '19012733888',
+            position: '专员'
+          }, {
+            name: '曦慧',
+            phone: '19012733888',
+            position: '专员'
+          }, {
+            name: '婧初',
+            phone: '19012733888',
+            position: '专员'
+          }, {
+            name: '婧馨',
+            phone: '19012733888',
+            position: '专员'
+          }
+        ]
       }
-      
-    ],
-    searchList: [
-      {
-        id: '1',
-        name: '张三',
-      }, {
-        id: '2',
-        name: '王五',
-      }, {
-        id: '3',
-        name: '赵三四',
-      }, {
-        id: '4',
-        name: '综合管理部',
-      }, {
-        id: '5',
-        name: '市场部',
-      }
-
     ]
   },
 
@@ -51,83 +144,71 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
-  },
 
-  showInput: function () {
-    this.setData({
-      inputShowed: true
-    });
   },
-  hideInput: function () {
+  // 展示详情
+  slideDetail: function (e) {
+    var id = e.currentTarget.id,
+      list = this.data.orgData;
+    // 每次点击都将当前open换为相反的状态并更新到视图，视图根据open的值来切换css
+    for (var i = 0, len = list.length; i < len; ++i) {
+      if (list[i].code == id) {
+        list[i].open = !list[i].open;
+      } else {
+        list[i].open = false;
+      }
+    }
     this.setData({
-      inputVal: "",
-      inputShowed: false
+      orgData: list
     });
-  },
-  clearInput: function () {
-    this.setData({
-      inputVal: ""
-    });
-  },
-  inputTyping: function (e) {
-    this.setData({
-      inputVal: e.detail.value
-    });
-  },
-  emp: function (e) {
-    var id = e.currentTarget.id;
-    wx.navigateTo({
-      url: '../emp/emp?id=' + id
-    }) 
   }
 })
