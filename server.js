c5c82fcba0b1360164b76de32ad01669357822bc
yandixuan <451868992@qq.com>
2018-06-21 18:26:50 +0800

    update

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ export const OLD_BASE_URL = "http://10.165.3.100:99";
 //export const OLD_BASE_URL = "http://10.165.3.100:99";
 
 //正式机
-export const NEW_BASE_URL = "https://weixin.anyuanhb.com/web-service";
-export const OLD_BASE_URL = "http://10.165.3.99:99";
+// export const NEW_BASE_URL = "https://weixin.anyuanhb.com/web-service";
+// export const OLD_BASE_URL = "http://10.165.3.99:99";
 
 //im websocket
 export const BOSH_SERVICE = 'wss://weixin.anyuanhb.com/ws/';
